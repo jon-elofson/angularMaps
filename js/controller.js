5552d219f5d88eaeecb9bca85600331eb2deb862
jon-elofson <jonathan.elofson@gmail.com>
2015-10-02 18:07:39 -0700

    add search functionality

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,22 @@
-angular.module('app').controller("MapController", function($scope){
-  var mapOptions = {
-      zoom: 12,
-      center: new google.maps.LatLng(37.76,-122.4167),
-  };
-  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+angular.module('app').controller('MapController', function($scope, Map) {
+
+    $scope.place = {};
+
+    $scope.search = function() {
+        Map.search($scope.placeName)
+        .then(
+            function(res) { // success
+                Map.addMarker(res);
+                $scope.place.name = res.name;
+                $scope.place.lat = res.geometry.location.lat();
+                $scope.place.lng = res.geometry.location.lng();
+            },
+            function(status) { // error
+                $scope.apiError = true;
+                $scope.apiStatus = status;
+            }
+        );
+    };
+
+    Map.init();
 });

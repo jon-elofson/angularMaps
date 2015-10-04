5b7933fe9c428713af9b73f4497f065bb3bc7a99
jon-elofson <jonathan.elofson@gmail.com>
2015-10-03 22:13:04 -0700

    add yelp functionality

--- js/controller.js
+++ js/controller.js
@@ -2,20 +2,8 @@ angular.module('app').controller('MapController', function($scope, Map) {
 
     $scope.place = {};
 
-    $scope.search = function() {
-        Map.search($scope.placeName)
-        .then(
-            function(res) { // success
-                Map.addMarker(res);
-                $scope.place.name = res.name;
-                $scope.place.lat = res.geometry.location.lat();
-                $scope.place.lng = res.geometry.location.lng();
-            },
-            function(status) { // error
-                $scope.apiError = true;
-                $scope.apiStatus = status;
-            }
-        );
+    $scope.searchPlaces = function() {
+        Map.searchPlaces($scope.placeName);
     };
 
     Map.init();
--- js/service.js
+++ js/service.js
@@ -1,6 +1,7 @@
-angular.module('app').service('Map', function($q) {
+angular.module('app').service('Map', function() {
 
-    this.init = function() {
+
+    this.init = function () {
         var options = {
             center: new google.maps.LatLng(37.7833, -122.4167),
             zoom: 12,
@@ -10,30 +11,66 @@ angular.module('app').service('Map', function($q) {
             document.getElementById("map"), options
         );
         this.places = new google.maps.places.PlacesService(this.map);
+        this.markers = [];
     };
 
-    this.search = function(str) {
-      debugger;
-        var d = $q.defer();
-        this.places.textSearch({query: str}, function(results, status) {
-            if (status == 'OK') {
-                d.resolve(results[0]);
+    this.randomString = function (length,chars) {
+      var result = '';
+      for (var i = length; i > 0; --i) {
+        result += chars[Math.round(Math.random() * (chars.length - 1))];
+      }
+      return result;
+    };
+
+
+    this.makeSignature = function () {
+      var httpMethod = 'GET',
+      url = 'https://api.yelp.com/v2/search',
+      parameters = {
+          oauth_consumer_key : 'CiqdJ5Ksityg9qxUloU6ig',
+          oauth_token : '8X72SYOQqLD-CypoTTMOu8at_ZxRK4r4',
+          oauth_nonce : this.randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+          oauth_timestamp : new Date().getTime(),
+          oauth_signature_method : 'HMAC-SHA1',
+          oauth_version : '1.0',
+      },
+      consumerSecret = 'DsAEfZYL50Tc1-ditW5LJqnpdz4',
+      tokenSecret = 'e-SGj9tCRfWkIDJDLIwy8_ALRak',
+      encodedSignature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret),
+      signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret,
+          { encodeSignature: false});
+
+    };
+
+    this.yelpSearch = function () {
+
+    };
+
+    this.searchPlaces = function(str) {
+      var that = this;
+      this.resetMarkers();
+      this.yelpSearch();
+      this.places.textSearch({query: str}, function(results, status) {
+          if (status === "OK") {
+            for (var i = 0; i < results.length; i++) {
+              that.addMarker(results[i]);
             }
-            else d.reject(status);
+          }
         });
-        return d.promise;
     };
 
-    this.addMarker = function(res) {
-        if (this.marker) {
-          this.marker.setMap(null);
-        }
-        this.marker = new google.maps.Marker({
+    this.resetMarkers = function () {
+      this.markers.forEach(function (marker) {
+        marker.setMap(null);
+      });
+    };
+
+    this.addMarker = function(place) {
+        this.markers.push(new google.maps.Marker({
             map: this.map,
-            position: res.geometry.location,
+            position: place.geometry.location,
             animation: google.maps.Animation.DROP
-        });
-        this.map.setCenter(res.geometry.location);
+        }));
     };
 
 });
